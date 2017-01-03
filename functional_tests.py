fd09e9bdcdb11072eb338144b1fd152182babe8f
codelieche <codelieche@gmail.com>
2017-01-04 00:08:05 +0800

    modified:   functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,24 @@
 #coding=utf-8
+'''
+function test
+'''
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 打开网址首页
+        self.browser.get('http://localhost:8000')
+        self.browser.implicitly_wait(3)
+
+        #网址标题是否包含'To-Do'
+        self.assertIn('To-Do', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main()
