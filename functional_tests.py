5d7140c1a97122295f2077dd8b256238f635b813
codelieche <codelieche@gmail.com>
2017-01-05 21:51:57 +0800

    Functional test add function check_for_row_in_list_table

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 打开网址首页
         self.browser.get('http://localhost:8000')
@@ -38,13 +43,8 @@ class NewVisitorTest(unittest.TestCase):
         # 她按回车，页面更新
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-        #     any(row.text == '1: Buy peacock feathers' for row in rows),
-        #     "New to-do item did not appear in table"
-        # )
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        # 检查
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面中又显示了一个文本框，可以输入其它待办事项
         # 她输入 "User peocock feathers to make a fly"
@@ -53,12 +53,9 @@ class NewVisitorTest(unittest.TestCase):
         # 她按回车，页面更新
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '2: User peacok feathers to make a fly',
-            [row.text for row in rows]
-        )
+        # 检查
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: User peacock feathers to make a fly')
 
         self.fail('Finish the test')
 
