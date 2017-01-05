19739f5077011ed7f1475bc8859cfb576348d6bb
codelieche <codelieche@gmail.com>
2017-01-05 17:08:16 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -9,7 +9,7 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(1)
 
     def tearDown(self):
         self.browser.quit()
@@ -40,12 +40,13 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # 页面中又显示了一个文本框，可以输入其它待办事项
         # 她输入 "User peocock feathers to make a fly"
-        
+
         self.fail('Finish the test')
 
 if __name__ == '__main__':
