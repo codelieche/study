b6167caa09310448fca93f324f48afde140663b0
codelieche <codelieche@gmail.com>
2017-01-28 19:45:54 +0800

    Refactor list view to handle new item POSTs

--- lists/views.py
+++ lists/views.py
@@ -18,6 +18,9 @@ def home_page(request):
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     # items = Item.objects.filter(list=list_)
+    if request.method == "POST":
+        Item.objects.create(text=request.POST.get('item_text', ''), list=list_)
+        return redirect('/lists/%d/' % list_.id)
     return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
@@ -35,10 +38,10 @@ def new_list(request):
             return render(request, 'home.html', {'error': error})
         return redirect('/lists/%d/' % list_.id)
 
-def add_item(request, list_id):
-    if request.method == 'POST':
-        new_item_text = request.POST.get('item_text', '')
-        list_ = List.objects.get(pk=list_id)
-        if new_item_text: Item.objects.create(text=new_item_text, list=list_)
-        return redirect('/lists/%d/' % list_.id)
+# def add_item(request, list_id):
+#     if request.method == 'POST':
+#         new_item_text = request.POST.get('item_text', '')
+#         list_ = List.objects.get(pk=list_id)
+#         if new_item_text: Item.objects.create(text=new_item_text, list=list_)
+#         return redirect('/lists/%d/' % list_.id)
 
