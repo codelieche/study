def429868e2750e15e04510de649ec91aae66b20
codelieche <codelieche@gmail.com>
2017-01-30 14:10:10 +0800

    end_of_chapter_14

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,6 +8,9 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
     
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+        
     # @skip
     def test_cannot_add_empty_list_items(self):
         # yds访问首页，不小心提交了一个空的待办事项
@@ -49,5 +52,21 @@ class ItemValidationTest(FunctionalTest):
         self.check_for_row_in_list_table('1: Buy wellies')
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You're already got this in your list")
+
+    def test_error_messages_are_cleared_on_input(self):
+        # yds新建一个清单，但方法不当，所以出现了一个验证错误
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys(' \n')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertTrue(error.is_displayed())
+
+        # 为了消除错误，她开始在输入框中输入内容
+        self.get_item_input_box().send_keys('a')
+
+        # 看到错误消息消失了，她很高兴
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
+
+
 
 
