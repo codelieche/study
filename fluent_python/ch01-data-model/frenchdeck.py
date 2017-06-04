3f7c9ac22943105f936743a0871832507e7b9013
codelieche <codelieche@gmail.com>
2017-06-04 18:33:50 +0800

    Add FrenchDeck Class.

--- fluent_python/ch01-data-model/frenchdeck.py
+++ fluent_python/ch01-data-model/frenchdeck.py
@@ -12,6 +12,42 @@ def demo_01():
     print(u2.name, u2.age)
     print(u2[0], u2[1])
 
+# 定义命名元组类：Card
+# Card有两个属性，一个是花色，一个是数值大小
+Card = namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    # 扑克牌类
+    # rank: 1-10 JKQA, suit：黑红方梅（spades, hearts, clubs, diamonds）
+    ranks = [str(i) for i in range(2, 11)] + list("JKQA")
+    suits = "spades hearts clubs diamonds".split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for rank in self.ranks
+                       for suit in self.suits]
+
+    def __len__(self):
+        # 返回所有牌的长度
+        # 使用len(obj)返回长度
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        # 根据序号返回对应的扑克牌
+        # 使用obj[n] 返回对应的扑克牌
+        # n可以是正数也可以上负数，但是不能超出len-1
+        return self._cards[position]
+
 
 if __name__ == "__main__":
-    demo_01()
+    # nametuple simple demo
+    # demo_01()
+
+    # French Card demo
+    obj = FrenchDeck()
+    print(len(obj))  # 52
+    print(obj[27])  # Card(rank='8', suit='diamonds')
+    print(obj[-1])  # Card(rank='A', suit='diamonds')
+    # print(obj[55])  # IndexError: list index out of range
+
+
