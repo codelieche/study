74e5906c9c557654cb20a9b310d39bbbf6a4d0c7
codelieche <codelieche@gmail.com>
2019-02-13 12:04:39 +0800

    get image url and show

--- JavaScript/scrapy-images/js/popup.js
+++ JavaScript/scrapy-images/js/popup.js
@@ -1,20 +1,41 @@
 $(function(){
     // 给各按钮注册事件
     $("#copy").click(function(){
-        console.log("点击了复制按钮");
+        // console.log("点击了复制按钮");
         var query = { active: true, currentWindow: true };
         chrome.tabs.query(query, function(tabs) {
             let tab = tabs[0];
-            // for(var i in tab){
-            //     alert(i);
-            // }
-            alert("点击了复制按钮,当前tab ID为：" + tab.id);
+            alert(`点击了复制按钮,当前tab ID为：${tab.id},url为：${tab.url}`);
         });
     });
 
     $("#scrapy").click(function(){
-        console.log("点击了爬取图片按钮");
+        // console.log("点击了爬取图片按钮");
+        var query = {active: true, currentWindow: true};
+        // 获取chrome的当前标签页
+        chrome.tabs.query(query, function(tabs){
+            var currentTab = tabs[0];
+            // 发送一个消息出去
+            var info = {
+                job: "scrapy", 
+                url: currentTab.url,
+            }
+            chrome.tabs.sendMessage(currentTab.id, info, null, function(response){
+                // 这个回调函数接收返回值
+                // alert(response.title);
+                // alert(response.results);
+                var results = response.results;
+                for(var i=0; i <= results.length; i++){
+                    // alert(results[i]);
+                    var span1 = $("<span></span>").text(results[i]);
+                    var img1 = $("<img />").attr("src", results[i]);
+                    // alert(img1)
+                    $(".images").append(span1, img1);
+                }
+            });
+        });
     });
+
     $("#show").click(function(){
         console.log("点击了查看图片按钮");
     });
--- JavaScript/scrapy-images/js/scrapy.js
+++ JavaScript/scrapy-images/js/scrapy.js
@@ -1,1 +1,50 @@
-console.log("Scrapy Images Execute！");
+// 监听消息
+// chrome.runtime.onMessageExternal.addListener
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
+    if(request.job === "copy"){
+        alert("收到了copy的消息");
+    }
+
+    if(request.job === "scrapy"){
+        // alert("收到了scrapy的消息");
+        // alert(sender);
+        // alert(sendResponse);
+        
+        var results = ScrapyImages(request.url);
+
+        let response = {
+            title: "scrapy job response",
+            results: results
+        };
+        // alert(response.results);
+        sendResponse(response);
+    }
+
+    if(request.job === "show"){
+        alert("收到了show的消息");
+    }
+});
+
+
+
+function ScrapyImages(url){
+    var results = [];
+    $("img").each(function(){
+        // alert($(this).text());
+        var imageSrc = $(this).attr("src");
+        // 判断图片地址是否需要加host
+        if(imageSrc && (! imageSrc.startsWith("http")) ){
+            if(imageSrc.startsWith("//")){
+                imageSrc = imageSrc.replace("//", "http://");
+            }else{
+                var re = new RegExp("(htt.*?:\/\/.*?)\/.*");
+                var host = re.exec(url)[1];
+                imageSrc = host + imageSrc;
+            }
+            
+        }
+        results.push(imageSrc);
+    });
+
+    return results;
+}
